c218d021be0869e3302871d9683eb5b1416a88b9
Jean Peel <Jean.Peel.246@gmail.com>
2020-06-18 17:33:45 -0600

    added body parser

---
+++ server.js
@@ -0,0 +1,6 @@
+const htmlRoutes = require ('./app/routing/htmlRoutes');
+const apiRouts = require('./app/routing/apiRoutes');
+const friends = require('./app/data/friends');
+
+const express = require('express');
+const bodyParser = require('body-parser');
