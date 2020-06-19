9740dd551ccf51ca9253eb2a2899874120fc1f99
Jean Peel <Jean.Peel.246@gmail.com>
2020-06-18 21:46:54 -0600

    addded mirandas logic on apirouts

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,18 +6,45 @@ module.exports = function (app) {
     });
 
     app.post("/api/friends", function (req, res) {
-        res.json(req.body)
-
-        var i;
-
-        for (i = 0; i < friendsData.length; i++) {
-            console.log(friendsData[i])
-            console.log(req.body)
-          var totalScore = 0;
-            for (j = 0; j < friendsData[i].scores.length; j++) {
-               totalScore += friendsData[i].scores[j] = req.body.scores[j]
-            }
-            console.log(totalScore)
+        var newFriend = {
+            name: request.body.name,
+            photo: request.body.photo,
+            scores: []
         };
+
+        var scoresArray = [];
+        for (var i = 0; i < request.body.scores.length; i++)
+        {
+            scoresArray.push(parseInt(request.body.scores[i]))
+        };
+        newFriend.scores = scoresArray;
+
+        var compareScoresArray = [];
+        for (var i = 0; i < friends.length; i++)
+        {
+            var comparedScore = 0;
+            for (var m = 0; m < newFriend.scores.length; m++)
+            {
+                comparedScore += Math.abs(newFriend.scores[m] - friends[i].scores[m]);
+            }
+            compareScoresArray.push(comparedScore);
+        }
+
+        var bestMatch = 0;
+        for (var i = 1; i < compareScoresArray.length; i++)
+        {
+            if(compareScoresArray[i] <= compareScoresArray[bestMatch])
+            {
+                bestMatch = i;
+            }
+        }
+
+        var theBestestOfFriendsMatch = friends[bestMatch];
+
+        response.json(theBestestOfFriendsMatch);
+        console.log("Success! Friend Added!");
+
+        friends.push(newFriend);
+
     });
 }
