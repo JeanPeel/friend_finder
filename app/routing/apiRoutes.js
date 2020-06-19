2c17e111f5f9e6c9973e941757a28caaa2adb128
Jean Peel <Jean.Peel.246@gmail.com>
2020-06-18 18:45:13 -0600

    added api routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,3 +1,23 @@
-// A GET route with the url `/api/friends`
+const friends = require('../data/friends');
+const server = require('../../server');
+
+module.exports = function(app){
+    let user;
 
 // A POST routes `/api/friends`
+app.post('/api/friends', function (req, res) {
+    module.exports.user = req.body;
+    friends.array.push(req.body);
+    console.log(`${req.body.name} successfully added!`)
+});
+
+// A GET route with the url `/api/friends`
+app.get('/bestFriend', function (req, res) {
+    let bestfriend = server.lookingForFriends();
+    res.json(bestfriend);
+});
+app.get('/api/:friends?', function (req, res) {
+    res.json(friends.array);
+});
+}
+
