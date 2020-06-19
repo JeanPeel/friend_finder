ecfcc6c02e2ea45bd235ec1e86113303d90d8bf5
Jean Peel <Jean.Peel.246@gmail.com>
2020-06-18 19:47:38 -0600

    added avatar charectors

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,39 @@
 module.exports = {
     array: [{
-        "name":"Mr X",
-        "photo":"../images/mrX.jpg",
+        "name":"Zuko",
+        "photo":"https://vignette.wikia.nocookie.net/avatar/images/4/4b/Zuko.png/revision/latest?cb=20180630112142",
+        "scores":[
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+          ]
+    },
+    {
+        "name":"Toph Beifong",
+        "photo":"https://vignette.wikia.nocookie.net/avatar/images/4/46/Toph_Beifong.png/revision/latest?cb=20131230122047",
+        "scores":[
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2
+          ]
+    },
+    {
+        "name":"Sokka",
+        "photo":"https://vignette.wikia.nocookie.net/avatar/images/c/cc/Sokka.png/revision/latest?cb=20140905085428",
         "scores":[
             3,
             3,
@@ -13,6 +45,38 @@ module.exports = {
             3,
             3,
             3
+          ]
+    },
+    {
+        "name":"Aang",
+        "photo":"https://vignette.wikia.nocookie.net/avatar/images/a/ae/Aang_at_Jasmine_Dragon.png/revision/latest?cb=20130612174003",
+        "scores":[
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3
+          ]
+    },
+    {
+        "name":"Iroh",
+        "photo":"https://vignette.wikia.nocookie.net/avatar/images/c/c1/Iroh_smiling.png/revision/latest?cb=20130626131914",
+        "scores":[
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5
           ]
     }
     ]
