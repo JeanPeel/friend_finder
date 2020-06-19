1223dd258c4fdac39f7b782cc97c072f5a187d73
Jean Peel <Jean.Peel.246@gmail.com>
2020-06-18 18:20:31 -0600

    added mr x

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,16 +1,19 @@
-// {
-//     "name":"Ahmed",
-//     "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-//     "scores":[
-//         5,
-//         1,
-//         4,
-//         4,
-//         5,
-//         1,
-//         2,
-//         5,
-//         4,
-//         1
-//       ]
-//   }
+module.exports = {
+    array: [{
+        "name":"MrX",
+        "photo":"../images/mrX.jpg",
+        "scores":[
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3
+          ]
+    }
+    ]
+  }
