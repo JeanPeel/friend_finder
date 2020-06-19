4eb2fc299e900105e103a3dd76997c0bff08177e
Jean Peel <Jean.Peel.246@gmail.com>
2020-06-19 14:32:28 -0600

    switched to nilsens logic on api routs

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,49 +6,75 @@ module.exports = function (app) {
     });
 
     app.post("/api/friends", function (req, res) {
-        var newFriend = {
-            name: request.body.name,
-            photo: request.body.photo,
-            scores: []
+
+        res.json(req.body)
+
+        // var newFriend = {
+        //     name: request.body.name,
+        //     photo: request.body.photo,
+        //     scores: []
+        // };
+
+        console.log('Body: ' + req.body);
+
+        var i;
+
+        //Outer loop to access individual friends
+        for (i = 0; i < friendsData.length; i++) {
+            console.log('Friends Data of i: ' + friendsData[i])
+            console.log('Body2: ' + req.body)
+        //Inner loop to access friends' scores
+          //  friendsData[i];
+          //  friendsData[i].scores
+          var totalScore = 0;
+            for (j = 0; j < friendsData[i].scores.length; j++) {
+               totalScore += friendsData[i].scores[j] = req.body.scores[j]
+            }
+            console.log('Total Score: ' + totalScore)
         };
 
-        console.log('New  Friend: ' + newFriend)
+        // console.log('New Friend, name: ' + newFriend.name);
+        // console.log('New Friend, photo: ' + newFriend.photo);
+        // console.log('New Friend, scores: ' + newFriend.scores);
+        // console.log('New  Friend: ' + newFriend);
 
-        var scoresArray = [];
-        for (var i = 0; i < request.body.scores.length; i++) {
-            scoresArray.push(parseInt(request.body.scores[i]))
-        };
+        // var scoresArray = [];
+        // for (var i = 0; i < request.body.scores.length; i++) {
+        //     scoresArray.push(parseInt(request.body.scores[i]))
+        // };
 
-        // coming out as not defined
-        newFriend.scores = scoresArray;
-        console.log('Scores Array ' + scoresArray)
+        // // coming out as not defined
+        // newFriend.scores = scoresArray;
+        // console.log('Scores Array ' + scoresArray);
 
-        var compareScoresArray = [];
-        for (var i = 0; i < friends.length; i++) {
-            var comparedScore = 0;
-            for (var m = 0; m < newFriend.scores.length; m++) {
-                comparedScore += Math.abs(newFriend.scores[m] - friends[i].scores[m]);
-            }
-            compareScoresArray.push(comparedScore);
-            console.log('Compared Score: ' + comparedScore)
-        }
+        
 
-        var bestMatch = 0;
-        for (var i = 1; i < compareScoresArray.length; i++) {
-            if (compareScoresArray[i] <= compareScoresArray[bestMatch]) {
-                bestMatch = i;
-            }
-            console.log('Best Match: ' + bestMatch)
-        }
+        // var compareScoresArray = [];
+        // for (var i = 0; i < friends.length; i++) {
+        //     var comparedScore = 0;
+        //     for (var m = 0; m < newFriend.scores.length; m++) {
+        //         comparedScore += Math.abs(newFriend.scores[m] - friends[i].scores[m]);
+        //     }
+        //     compareScoresArray.push(comparedScore);
+        //     console.log('Compared Score: ' + comparedScore);
+        // }
 
-        var theBestestOfFriendsMatch = friends[bestMatch];
-        console.log('Best of Friends Match: ' + theBestOfFriendsMatch)
+        // var bestMatch = 0;
+        // for (var i = 1; i < compareScoresArray.length; i++) {
+        //     if (compareScoresArray[i] <= compareScoresArray[bestMatch]) {
+        //         bestMatch = i;
+        //     }
+        //     console.log('Best Match: ' + bestMatch);
+        // }
 
-        response.json(theBestestOfFriendsMatch);
-        console.log("Success! Friend Added!");
+        // var theBestestOfFriendsMatch = friends[bestMatch];
+        // console.log('Best of Friends Match: ' + theBestOfFriendsMatch)
 
-        friends.push(newFriend);
-        console.log('New Friend ' + newFriend)
+        // response.json(theBestestOfFriendsMatch);
+        // console.log("Success! Friend Added!");
+
+        // friends.push(newFriend);
+        // console.log('New Friend ' + newFriend);
 
     });
 }
