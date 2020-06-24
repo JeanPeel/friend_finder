abadf85faccbfea283a01def6d21185c996f488f
Jean Peel <Jean.Peel.246@gmail.com>
2020-06-24 03:02:46 -0600

    removing extras

--- app/data/pushData.js
+++
@@ -1,6 +0,0 @@
-var pushData = [
-]
-
-
-
-module.exports = pushData;
--- ready.js
+++
@@ -1,6 +0,0 @@
-//----- Trying some logic from Jacob
-  $(document).ready(function() {
-    alert('hi');
-  });
-  
-  console.log('hello!');
